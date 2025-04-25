5b9e372251053e0a207c7cee886ca6db5f6d9441
edmarcres18 <edmarcrescencio856@gmail.com>
2025-04-25 08:44:56 +0800

    Updated

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -237,7 +237,7 @@
                                     @php
                                         $unreadCareersCount = \App\Models\Career::where('is_read', false)->count();
                                     @endphp
-                                    <span id="unread-careers-badge" class="badge badge-danger right" style="{{ $unreadCareersCount > 0 ? '' : 'display: none;' }}">{{ $unreadCareersCount }}</span>
+                                    <span id="unread-careers-badge" class="badge badge-danger right {{ $unreadCareersCount > 0 ? '' : 'd-none' }}">{{ $unreadCareersCount }}</span>
                                 </p>
                             </a>
                         </li>
